45e274fd7bf3b7907e7903ee9283ce26a18735eb
qiyve <100032272+qiyve@users.noreply.github.com>
2022-11-26 02:24:57 +0800

    Update 远程DLL注入.cpp

--- 逆向/滴水三期/Win32/远程Dll注入/远程DLL注入.cpp
+++ 逆向/滴水三期/Win32/远程Dll注入/远程DLL注入.cpp
@@ -13,22 +13,19 @@ VOID LoadDll(DWORD Did, char* PathName)
 	//打开本地进程对象  参数：指定所得句柄具有的访问权限、 是否可被继承、 指定要打开的进程ID
 	HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, Did);
  
-
 	//2、计算Dll名字长度  strlen 返回的字符串长度不包括 \0 
 	DWORD dwLength = strlen(PathName) + 1;
  
-
 	//3、给目标进程申请空间
 	//	参数：1.申请内存所在的进程句柄 、2.保留页面的内存地址(一般用NULL)、3.预分配内存大小
 	//		  4.分配内存空间的类型 、 5.内存访问权限 PAGE_READWRITE 可读可写
 	LPSTR FileRemote = (LPSTR)VirtualAllocEx(hProcess, NULL, dwLength, MEM_COMMIT, PAGE_READWRITE);
 
- 
+
 	//4、拷贝DLL进去  WriteProcessMemory将数据写入指定进程中的内存区域
 	//	参数：目标进程句柄、目标进程内存空间首地址、需要写入数据的内存空间地址、
 	//	需要写入数据字节数、实际写入的字节数，设置为NULL
 	WriteProcessMemory(hProcess, (LPVOID)FileRemote, (LPVOID)PathName, dwLength, NULL);
-
 
 	//5、获取模块地址 
 	//GetModuleHandleA 检索指定模块的模块句柄。该模块必须已由调用进程加载。
